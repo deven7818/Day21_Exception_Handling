2e00d5c81632ad7d16eebd90c8b4e7277bdef007
deven7818 <deven7818@gmail.com>
2022-03-14 21:14:27 +0530

    first Name validation

---
+++ ExceptionHandling/src/main/java/com/exceptionhandling/UserRegistrationExMain.java
@@ -0,0 +1,25 @@
+package com.exceptionhandling;
+
+
+/**
+ * PRODEDURE
+ * User Registration 
+ * User Registration System needs to ensure all validations are in place during the User Entry
+ * 1. User needs to enter a valid First Name
+ * 
+ *
+ */
+public class UserRegistrationExMain {
+    public static void main( String[] args )
+    {
+    	/*
+    	 * Creating object of userDetails class
+    	 */
+    	UserRegistrationException user = new UserRegistrationException();
+    	
+    	/**
+    	 * calling firstName method to ensure valid first name
+    	 */
+    	user.firstName();
+    }
+}
---
+++ ExceptionHandling/src/main/java/com/exceptionhandling/UserRegistrationException.java
@@ -0,0 +1,32 @@
+package com.exceptionhandling;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistrationException {
+	
+	/*
+	 * Scanner class object to get value from user
+	 */
+	Scanner sc = new Scanner(System.in);
+	
+	
+	/**
+	 * Method to Enter FirstName
+	 * First Name should start with capital and has minimum 3 characters
+	 * if entered firstName matches the pattern then it gives true value
+	 * else it gives false Invalid first name
+	 */
+	public void firstName() {
+		System.out.println("Enter First Name : ");
+		String FirstName = sc.next();
+		boolean result = Pattern.matches("[A-Z][a-z]{2,}", FirstName);
+		if(result) 
+			System.out.println("Valid firstName");
+		else
+			System.out.println("Invalid... Please Enter Valid first name");
+	}
+	
+
+	
+}
